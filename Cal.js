168ef632c191fa8199292cc3a879c42cad17531d
foo9 <fu9fu9fu9@gmail.com>
2015-07-04 21:04:50 +0900

    Add some method.

--- Cal.js
+++ Cal.js
@@ -1,28 +1,61 @@
 var Calendar;
 (function (Calendar) {
-    var DateEntity = (function () {
-        function DateEntity(date, focus, today) {
-            if (focus === void 0) { focus = false; }
+    var Cell = (function () {
+        function Cell(date, active, today) {
+            if (active === void 0) { active = false; }
             if (today === void 0) { today = false; }
             this._date = new Date(date.getTime());
-            this._isFocusMonth = focus;
+            this._isActiveMonth = active;
             this._isToday = today;
         }
-        DateEntity.prototype.toDate = function () {
+        Cell.prototype.toDate = function () {
             return new Date(this._date.getTime());
         };
-        DateEntity.prototype.isFocusMonth = function () {
-            return this._isFocusMonth;
+        Cell.prototype.isActiveMonth = function () {
+            return this._isActiveMonth;
         };
-        DateEntity.prototype.isToday = function () {
+        Cell.prototype.isToday = function () {
             return this._isToday;
         };
-        DateEntity.prototype.toString = function () {
-            return (this._date.getMonth() + 1) + '/' + this._date.getDate();
+        Cell.prototype.getYear = function () {
+            return this._date.getFullYear();
         };
-        return DateEntity;
+        Cell.prototype.getMonth = function () {
+            return this._date.getMonth() + 1;
+        };
+        Cell.prototype.getDate = function () {
+            return this._date.getDate();
+        };
+        Cell.prototype.isFirstDateOfMonth = function () {
+            return this._date.getDate() === 1;
+        };
+        Cell.prototype.getLastDateOfMonth = function () {
+            var d = this._date;
+            return (new Date(d.getFullYear(), d.getMonth() + 1, 0)).getDate();
+        };
+        Cell.prototype.isLastDateOfMonth = function () {
+            var current = this.getDate();
+            var last = this.getLastDateOfMonth();
+            return current === last;
+        };
+        Cell.prototype.isFirstWeekOfMonth = function () {
+            var current = this.getDate();
+            return (current % 7) === 0;
+        };
+        Cell.prototype.isLastWeekOfMonth = function () {
+            var current = this.getDate();
+            var last = this.getLastDateOfMonth();
+            return (current % 7) == (last % 7);
+        };
+        Cell.prototype.toString = function () {
+            var y = this.getYear();
+            var m = this.getMonth();
+            var d = this.getDate();
+            return [y, m, d].join('/');
+        };
+        return Cell;
     })();
-    Calendar.DateEntity = DateEntity;
+    Calendar.Cell = Cell;
     var Sheet = (function () {
         function Sheet(date, today) {
             if (date === void 0) { date = new Date(); }
@@ -30,6 +63,14 @@ var Calendar;
             this._pivotDate = new Date(date.getTime());
             this._todayDate = new Date(today.getTime());
         }
+        Sheet.prototype.setMonth = function (month) {
+            this._pivotDate = new Date(month.getTime());
+            return this;
+        };
+        Sheet.prototype.today = function () {
+            this._pivotDate.setMonth(this._todayDate.getMonth());
+            return this;
+        };
         Sheet.prototype.next = function () {
             this._pivotDate.setMonth(this._pivotDate.getMonth() + 1);
             return this;
@@ -38,23 +79,24 @@ var Calendar;
             this._pivotDate.setMonth(this._pivotDate.getMonth() - 1);
             return this;
         };
-        Sheet.prototype.toArray = function () {
+        Sheet.prototype.toArray = function (format) {
+            if (format === void 0) { format = function (c) { return c; }; }
             var date = new Date(this._pivotDate.getTime());
             var normalized = this.normalizeDate(date);
             normalized.setDate(1);
             normalized.setDate(normalized.getDate() - normalized.getDay());
             var weeks = [];
             var week = 0;
-            var isFocusMonth;
+            var isActiveMonth;
             var isToday;
             while (true) {
                 if (!weeks[week]) {
                     weeks[week] = [];
                 }
                 if (weeks[week].length !== Sheet.DAYS_IN_WEEK) {
-                    isFocusMonth = this.computeFocusMonth(normalized, date);
+                    isActiveMonth = this.isActiveMonth(normalized, date);
                     isToday = this.isSame(normalized, this._todayDate);
-                    weeks[week].push(new DateEntity(normalized, isFocusMonth, isToday));
+                    weeks[week].push(format(new Cell(normalized, isActiveMonth, isToday)));
                     normalized.setDate(normalized.getDate() + 1);
                 }
                 if (weeks.length === Sheet.WEEKS_IN_SHEET && weeks[week].length === Sheet.DAYS_IN_WEEK) {
@@ -76,10 +118,19 @@ var Calendar;
             var bDate = this.normalizeDate(new Date(b.getTime()));
             return ((aDate.getTime() - bDate.getTime()) === 0);
         };
-        Sheet.prototype.computeFocusMonth = function (a, b) {
+        Sheet.prototype.isActiveMonth = function (a, b) {
             var aIndex = (new Date(a.getTime())).getMonth();
             var bIndex = (new Date(b.getTime())).getMonth();
             return ((aIndex - bIndex) == 0);
+        };
+        Sheet.prototype.getYear = function () {
+            return this._pivotDate.getFullYear();
+        };
+        Sheet.prototype.getMonth = function () {
+            return this._pivotDate.getMonth() + 1;
+        };
+        Sheet.prototype.getDate = function () {
+            return this._pivotDate.getDate();
         };
         Sheet.DAYS_IN_WEEK = 7;
         Sheet.WEEKS_IN_SHEET = 6;
--- Cal.ts
+++ Cal.ts
@@ -1,147 +1,184 @@
 module Calendar {
-    export class DateEntity {
-        private _date: Date;
-        private _isFocusMonth: boolean;
-        private _isToday: boolean;
+	export class Cell {
+		private _date: Date;
+		private _isActiveMonth: boolean;
+		private _isToday: boolean;
 
-        constructor(date: Date, focus: boolean = false, today: boolean = false) {
-            this._date = new Date(date.getTime());
-            this._isFocusMonth = focus;
-            this._isToday = today;
-        }
+		constructor(date: Date, active: boolean = false, today: boolean = false) {
+			this._date = new Date(date.getTime());
+			this._isActiveMonth = active;
+			this._isToday = today;
+		}
 
-        public toDate(): Date {
-            return new Date(this._date.getTime());
-        }
+		toDate(): Date {
+			return new Date(this._date.getTime());
+		}
 
-        public isFocusMonth(): boolean {
-            return this._isFocusMonth;
-        }
+		isActiveMonth(): boolean {
+			return this._isActiveMonth;
+		}
 
-        public isToday(): boolean {
-            return this._isToday;
-        }
+		isToday(): boolean {
+			return this._isToday;
+		}
 
-        public toString(): any {
-            return (this._date.getMonth() + 1) + '/' + this._date.getDate();
-        }
-    }
+		getYear(): number {
+			return this._date.getFullYear();
+		}
 
-    export class Sheet {
-        public static DAYS_IN_WEEK: number = 7;
-        public static WEEKS_IN_SHEET: number = 6;
+		getMonth(): number {
+			return this._date.getMonth() + 1;
+		}
 
-        private _pivotDate: Date;
-        private _todayDate: Date;
+		getDate(): number {
+			return this._date.getDate();
+		}
 
-        constructor(date: Date = new Date(), today: Date = new Date()) {
-            this._pivotDate = new Date(date.getTime());
-            this._todayDate = new Date(today.getTime());
-        }
+		isFirstDateOfMonth(): boolean {
+			return this._date.getDate() === 1;
+		}
 
-        public next(): Sheet {
-            this._pivotDate.setMonth(this._pivotDate.getMonth() + 1);
+		getLastDateOfMonth(): number {
+			var d = this._date;
 
-            return this;
-        }
+			return (new Date(d.getFullYear(), d.getMonth() + 1, 0)).getDate();
+		}
 
-        public prev(): Sheet {
-            this._pivotDate.setMonth(this._pivotDate.getMonth() - 1);
+		isLastDateOfMonth(): boolean {
+			var current = this.getDate();
+			var last = this.getLastDateOfMonth();
 
-            return this;
-        }
+			return current === last;
+		}
 
-        public toArray(): Array<Array<DateEntity>> {
-            var date: Date = new Date(this._pivotDate.getTime());
+		isFirstWeekOfMonth(): boolean {
+			var current = this.getDate();
 
-            // 対象の月の、最初の日を取得
-            var normalized: Date = this.normalizeDate(date);
-            normalized.setDate(1);
-            // 対象の月の、最初の日の、その日の週初めの日を設定
-            normalized.setDate(normalized.getDate() - normalized.getDay());
+			return (current % 7) === 0;
+		}
 
-            var weeks: Array<Array<DateEntity>> = [];
-            var week: number = 0;
-            var isFocusMonth: boolean;
-            var isToday: boolean;
+		isLastWeekOfMonth(): boolean {
+			var current = this.getDate();
+			var last = this.getLastDateOfMonth();
 
-            while (true) {
-                if (!weeks[week]) {
-                    weeks[week] = [];
-                }
+			return (current % 7) == (last % 7);
+		}
 
-                if (weeks[week].length !== Sheet.DAYS_IN_WEEK) {
-                    isFocusMonth = this.computeFocusMonth(normalized, date);
-                    isToday = this.isSame(normalized, this._todayDate);
+		toString(): any {
+            var y = this.getYear();
+            var m = this.getMonth();
+            var d = this.getDate();
 
-                    weeks[week].push(new DateEntity(normalized, isFocusMonth, isToday));
-                    normalized.setDate(normalized.getDate() + 1);
-                }
+			return [y, m, d].join('/');
+		}
+	}
 
-                if (weeks.length === Sheet.WEEKS_IN_SHEET && weeks[week].length === Sheet.DAYS_IN_WEEK) {
-                    break;
-                }
+	export class Sheet {
+		static DAYS_IN_WEEK: number = 7;
+		static WEEKS_IN_SHEET: number = 6;
 
-                if (weeks[week].length === Sheet.DAYS_IN_WEEK) {
-                    week++;
-                }
-            }
+		private _pivotDate: Date;
+		private _todayDate: Date;
 
-            return weeks;
-        }
+		constructor(date: Date = new Date(), today: Date = new Date()) {
+			this._pivotDate = new Date(date.getTime());
+			this._todayDate = new Date(today.getTime());
+		}
 
-        public normalizeDate(d: Date): Date {
-            var normalized: Date = new Date(d.getTime());
-            normalized.setHours(0, 0, 0, 0);
+		setMonth(month: Date): Sheet {
+			this._pivotDate = new Date(month.getTime());
 
-            return normalized;
-        }
+			return this;
+		}
 
-        public isSame(a: Date, b: Date): boolean {
-            var aDate: Date = this.normalizeDate(new Date(a.getTime()));
-            var bDate: Date = this.normalizeDate(new Date(b.getTime()));
+		today(): Sheet {
+			this._pivotDate.setMonth(this._todayDate.getMonth());
 
-            return ((aDate.getTime() - bDate.getTime()) === 0);
-        }
+			return this;
+		}
 
-        public computeFocusMonth(a: Date, b: Date): boolean {
-            var aIndex: number = (new Date(a.getTime())).getMonth();
-            var bIndex: number = (new Date(b.getTime())).getMonth();
+		next(): Sheet {
+			this._pivotDate.setMonth(this._pivotDate.getMonth() + 1);
 
-            return ((aIndex - bIndex) == 0);
-        }
-        /*
-        public debug(): void {
-            var out: Array<string> = [];
-            var line: Array<string>;
+			return this;
+		}
 
-            this.toArray().forEach((r) => {
-                line = [];
-                r.forEach((e) => {
-                    var f: string = e.isFocusMonth() ? '' : '-';
-                    var t: string = e.isToday() ? '+' : '';
-                    line.push(e.toString() + f + t);
-                });
-                out.push(line.join('\t'));
-            });
+		prev(): Sheet {
+			this._pivotDate.setMonth(this._pivotDate.getMonth() - 1);
 
-            console.log(out.join('\n'));
-            console.log('----------------------------------------------------');
-        }
-        */
-    }
+			return this;
+		}
+
+		toArray(format: Function = (c: string) => { return c; }): Array<Array<Cell>> {
+			var date: Date = new Date(this._pivotDate.getTime());
+
+			// 対象の月の、最初の日を取得
+			var normalized: Date = this.normalizeDate(date);
+			normalized.setDate(1);
+			// 対象の月の、最初の日の、その日の週初めの日を設定
+			normalized.setDate(normalized.getDate() - normalized.getDay());
+
+			var weeks: Array<Array<Cell>> = [];
+			var week: number = 0;
+			var isActiveMonth: boolean;
+			var isToday: boolean;
+
+			while (true) {
+				if (!weeks[week]) {
+					weeks[week] = [];
+				}
+
+				if (weeks[week].length !== Sheet.DAYS_IN_WEEK) {
+					isActiveMonth = this.isActiveMonth(normalized, date);
+					isToday = this.isSame(normalized, this._todayDate);
+
+					weeks[week].push(format(new Cell(normalized, isActiveMonth, isToday)));
+					normalized.setDate(normalized.getDate() + 1);
+				}
+
+				if (weeks.length === Sheet.WEEKS_IN_SHEET && weeks[week].length === Sheet.DAYS_IN_WEEK) {
+					break;
+				}
+
+				if (weeks[week].length === Sheet.DAYS_IN_WEEK) {
+					week++;
+				}
+			}
+
+			return weeks;
+		}
+
+		normalizeDate(d: Date): Date {
+			var normalized: Date = new Date(d.getTime());
+			normalized.setHours(0, 0, 0, 0);
+
+			return normalized;
+		}
+
+		isSame(a: Date, b: Date): boolean {
+			var aDate: Date = this.normalizeDate(new Date(a.getTime()));
+			var bDate: Date = this.normalizeDate(new Date(b.getTime()));
+
+			return ((aDate.getTime() - bDate.getTime()) === 0);
+		}
+
+		isActiveMonth(a: Date, b: Date): boolean {
+			var aIndex: number = (new Date(a.getTime())).getMonth();
+			var bIndex: number = (new Date(b.getTime())).getMonth();
+
+			return ((aIndex - bIndex) == 0);
+		}
+
+		getYear(): number {
+			return this._pivotDate.getFullYear();
+		}
+
+		getMonth(): number {
+			return this._pivotDate.getMonth() + 1;
+		}
+
+		getDate(): number {
+			return this._pivotDate.getDate();
+		}
+	}
 }
-/*
-var sheet: Calendar.Sheet = new Calendar.Sheet();
-sheet.debug();
-
-sheet.next();
-sheet.debug();
-
-sheet.prev();
-sheet.prev();
-sheet.debug();
-
-sheet.next();
-sheet.debug();
-*/
